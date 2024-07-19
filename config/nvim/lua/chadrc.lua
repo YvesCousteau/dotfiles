2e5a5a6a024577d4947f9188c13c3946a3481396
YvesCousteau <hugohenrotte@hotmail.com>
2024-07-19 19:13:27 +0200

    hugeeeeeeeeeee update close of the my first rice

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -1,16 +1,71 @@
 -- This file  needs to have same structure as nvconfig.lua 
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
+vim.opt.termguicolors = true
+
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+  changed_themes = {
+    nord = {
+      base_16 = {
+        base00 = "#141929",
+        base01 = "#141929",
+        base02 = "#D4DFED", -- disable by visual
+        base03 = "#68818D",
+        base04 = "#D4DFED",
+        base05 = "#D4DFED",
+        base06 = "#0000FF",
+        base07 = "#BF48EA",
+        base08 = "#D4DFED",
+        base09 = "#95D473",
+        base0A = "#FE9D5D",
+        base0B = "#FF85A1",
+        base0C = "#7EBDC2",
+        base0D = "#FFEE93",
+        base0E = "#D295F6",
+        base0F = "#76A4E5",
+      },
+      base_30 = {
+        white = "#D4DFED",
+        darker_black = "#141929",
+        black = "#141929", --  nvim bg
+        black2 = "#141929",
+        one_bg = "#D4DFED", -- real bg of onedark
+        one_bg2 = "#00FFFF",
+        one_bg3 = "#D295F6",
+        grey = "#FF85A1",
+        grey_fg = "#68818D",
+        grey_fg2 = "#6e6f79",
+        light_grey = "#D4DFED",
+        red = "#F66C51",
+        baby_pink = "#FFFF00",
+        pink = "#FF00FF",
+        line = "#D4DFED", -- for lines like vertsplit
+        green = "#D4DFED",
+        vibrant_green = "#AA00AA",
+        nord_blue = "#D4DFED",
+        blue = "#D4DFED",
+        yellow = "#FFEE93",
+        sun = "#AA00FF",
+        purple = "#FFAA00",
+        dark_purple = "#D295F6",
+        teal = "#FF00AA",
+        orange = "#0000AA",
+        cyan = "#7EBDC2",
+        statusline_bg = "#141929",
+        lightbg = "#68818D",
+        pmenu_bg = "#D4DFED",
+        folder_bg = "#D4DFED",
+      },
+    },
+  },
+	theme = "nord",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+	   Visual = { fg = "#141929", bg = "#D4DFED" },
+	},
 }
 
 return M
